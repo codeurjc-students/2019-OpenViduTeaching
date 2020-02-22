ae4861f59e2924c4b0569bf50224d3c4970ef061
diegomzmn <diego_mzmn@hotmail.com>
2020-02-23 00:29:01 +0100

    Jar generation

--- docker/create_image.cmd
+++ docker/create_image.cmd
@@ -3,10 +3,19 @@ cd ../frontend
 :: Build angular app
 :: docker run --rm --name angular-cli -v ${PWD}:/angular -w /angular node /bin/bash -c "npm install; npm run ng build --prod --baseHref=http://localhost:8080/"
 call npm install
+call npm audit fix
 call npm run ng build --prod --baseHref=http://localhost:8080/
 
 :: Copy generated resources on static
-del /q ..\backend\src\main\resources\static\*
 xcopy /s /y dist\openvidu-teaching\* ..\backend\src\main\resources\static
 
+cd ../backend
+
+docker run -it --rm -v "%cd%":/usr/src/project -w /usr/src/project maven:alpine mvn -Dmaven.test.skip=true package
+xcopy /y target\OpenViduTeaching-1.0.0.jar ..\docker\build\
+
 cd ../docker
+
+:: docker build -t diegomzmn/openviduteaching .
+cd build
+java -jar OpenViduTeaching-1.0.0.jar
