12dbaf52954c289823bf5a2a3c37ac2f4063472e
diegomzmn <diego_mzmn@hotmail.com>
2020-03-07 23:43:17 +0100

    Standalone docker updated + build arg in composed removed

--- docker/composed/build_composed.cmd
+++ docker/composed/build_composed.cmd
@@ -1,7 +1,5 @@
 xcopy /y ..\build\OpenViduTeaching-1.0.0.jar .
-xcopy /y ..\build\initialData.json .
 :: Build docker image
-docker build --build-arg initialDataFile=initialData.json -t diegomzmn/openviduteaching .
+docker build -t diegomzmn/openviduteaching .
 :: docker push diegomzmn/openviduteaching
 del /q OpenViduTeaching-1.0.0.jar
-del /q initialData.json
--- docker/dockerTeachingStandalone.bat
+++ docker/dockerTeachingStandalone.bat
@@ -1,2 +1,2 @@
 docker kill openviduteaching-standalone
-docker run --name openviduteaching-standalone -p 4443:4443 -p 8080:8080 --rm -e openvidu.secret=MY_SECRET diegomzmn/openviduteaching-standalone
+docker run --name openviduteaching-standalone -p 4443:4443 -p 8080:8080 -e openvidu.secret=MY_SECRET -e initialDataFile="/initialData.json" -v %cd%\build\initialData.json:/initialData.json --rm diegomzmn/openviduteaching-standalone
