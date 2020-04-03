f739d2b6980414cb05750dfe8af32aaff46a2ee0
diegomzmn <diego_mzmn@hotmail.com>
2020-04-03 19:18:54 +0200

    Copy to clipboard button on invite-link component

--- frontend/src/app/invite/invite-link/invite-link.component.ts
+++ frontend/src/app/invite/invite-link/invite-link.component.ts
@@ -33,4 +33,18 @@ export class InviteLinkComponent implements OnInit {
     this.data.dismiss();
   }
 
+  private copyToClipboard() {
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = this.url;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
 }
