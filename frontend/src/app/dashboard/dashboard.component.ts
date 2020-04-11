162c0c9407bfe6ae41319d9a24293422c479db17
diegomzmn <diego_mzmn@hotmail.com>
2020-04-11 20:46:40 +0200

    Drawer with videos in dashboard (with mockup data)

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { UserService } from './../shared/services/user.service';
 import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { Room } from '../shared/models/room-model';
-import { MatSnackBar, MatDialogRef, MatDialog } from '@angular/material';
+import { MatSnackBar, MatDialogRef, MatDialog, MatDrawer } from '@angular/material';
 import { InviteLinkComponent } from '../invite/invite-link/invite-link.component';
 
 @Component({
@@ -19,13 +19,17 @@ export class DashboardComponent implements OnInit {
   private presentedRooms: Room[] = [];
   private participatedRooms: Room[] = [];
 
+  private currentVideos: string[] = ["video1", "video2", "video3"];
+  private currentRoomOfVideos: string;
+
   private loading: boolean;
   private roomInputOpen: boolean;
   private newRoom: string;
 
   @ViewChild('addRoomDialog') addRoomDialog: TemplateRef<any>;
   dialogRef: MatDialogRef<any, any>;
-
+  @ViewChild('videosDrawer') videosDrawer: MatDrawer;
+  
   constructor(
     private router: Router,
     private userSrv: UserService,
@@ -51,6 +55,18 @@ export class DashboardComponent implements OnInit {
       },
       error => console.log(error)
     );
+  }
+
+  openVideos(roomName: string) {
+    const wasOpened = this.videosDrawer.opened;
+    this.videosDrawer.close();
+    if(this.currentRoomOfVideos !== roomName) {
+      this.currentRoomOfVideos = roomName;
+      this.currentVideos.push(this.currentRoomOfVideos); /**TODO change*/
+      this.videosDrawer.open();
+    } else if(!wasOpened) {
+      this.videosDrawer.open();
+    }
   }
 
   getInviteURL(roomName: string, role:string){
