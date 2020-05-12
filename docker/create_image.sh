817caec961c7c782dfc3d35bdb93f5d349d226e0
Diego <diego_mzmn@hotmail.com>
2020-05-12 19:52:44 +0200

    Create image Linux script

---
+++ docker/create_image.sh
@@ -0,0 +1,18 @@
+cd ../frontend
+npm install
+npm audit fix
+npm run build --prod --baseHref=http://localhost:8080/
+cd ../backend
+mvn -Dmaven.test.skip=true package
+cp target/OpenViduTeaching.jar ../docker/build/
+cd ../docker/build/
+cp OpenViduTeaching.jar ../standalone/
+cd ../standalone/
+docker build -t diegomzmn/openviduteaching-standalone .
+rm OpenViduTeaching.jar
+cd ../build
+cp OpenViduTeaching.jar ../composed/
+cd ../composed/
+docker build -t diegomzmn/openviduteaching .
+rm OpenViduTeaching.jar
+cd ../
