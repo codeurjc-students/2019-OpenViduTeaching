e46bbf335b232075d3f7eae37a1fb707e13f5a51
diegomzmn <diego_mzmn@hotmail.com>
2020-05-25 21:57:59 +0200

    Update home component to match old dashboard component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator';
+import { MatDrawer } from '@angular/material/sidenav';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Room } from '../shared/models/room-model';
+import { UserService } from '../shared/services/user/user.service';
+import { RoomService } from '../shared/services/room/room.service';
+import { InviteLinkComponent } from '../invite/invite-link/invite-link.component';
+import { RecordingService } from '../shared/services/recording/recording.service';
 
 @Component({
 	selector: 'app-home',
@@ -9,21 +15,129 @@ import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-
 	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-	public roomForm: FormControl;
 	public version = require('../../../package.json').version;
 
-	constructor(private router: Router, public formBuilder: FormBuilder) {}
+	moddedRooms: Room[] = [];
+	presentedRooms: Room[] = [];
+	participatedRooms: Room[] = [];
+
+	private isRecordingEnabled: boolean = false;
+	currentVideos: { id: string; name: string; createdAt: Date }[] = [];
+	currentRoomOfVideos: string;
+
+	loading: boolean;
+	private roomInputOpen: boolean;
+	private newRoom: string;
+
+	@ViewChild('addRoomDialog') addRoomDialog: TemplateRef<any>;
+	dialogRef: MatDialogRef<any, any>;
+	@ViewChild('videosDrawer') videosDrawer: MatDrawer;
+
+	constructor(
+		private router: Router,
+		private userSrv: UserService,
+		private roomSrv: RoomService,
+		private recordingSrv: RecordingService,
+		private urlSnackBar: MatSnackBar,
+		public dialog: MatDialog
+	) {}
 
 	ngOnInit() {
-		const randomName = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals], separator: '-', });
-		this.roomForm = new FormControl(randomName, [Validators.minLength(4), Validators.required]);
+		this.loggedIn();
 	}
 
-	public goToVideoCall() {
-		if (this.roomForm.valid) {
-			const roomName = this.roomForm.value.replace(/ /g, '-'); // replace white spaces by -
-			this.roomForm.setValue(roomName);
-			this.router.navigate(['/', roomName]);
+	loggedIn() {
+		if (this.userSrv.isLogged) {
+			this.getRooms();
+			this.checkIsRecordingEnabled();
 		}
+	}
+
+	getRooms() {
+		this.loading = true;
+		this.userSrv.getRoomsForUser().subscribe(
+			(rooms) => {
+				this.moddedRooms = rooms.modded;
+				this.presentedRooms = rooms.presented;
+				this.participatedRooms = rooms.participated;
+				this.loading = false;
+			},
+			(error) => console.log(error)
+		);
+	}
+
+	private checkIsRecordingEnabled() {
+		this.recordingSrv.isRecordingEnabled().subscribe(
+			(isRecordingEnabled) => {
+				this.isRecordingEnabled = isRecordingEnabled;
+			},
+			(error) => console.error(error)
+		);
+	}
+
+	openVideos(roomName: string) {
+		this.currentVideos = [];
+		this.recordingSrv.getRecordings(roomName).subscribe(
+			(recordings) => {
+				this.currentRoomOfVideos = roomName;
+				for (let recording of recordings) {
+					if (recording.status == 'ready') {
+						this.currentVideos.push({
+							id: recording.id,
+							name: recording.name,
+							createdAt: new Date(recording.createdAt)
+						});
+					}
+				}
+				this.videosDrawer.open();
+			},
+			(error) => console.log(error)
+		);
+	}
+
+	getInviteURL(roomName: string, role: string) {
+		this.urlSnackBar.openFromComponent(InviteLinkComponent, {
+			data: {
+				roomName: roomName,
+				role: role,
+				dismiss: () => {
+					this.urlSnackBar.dismiss();
+				}
+			}
+		});
+	}
+
+	openAddRoomDialog() {
+		this.dialogRef = this.dialog.open(this.addRoomDialog, {
+			width: '50%',
+			height: '60%'
+		});
+	}
+
+	eventKeyPress(event: any) {
+		if (event && event.keyCode === 13) {
+			// Press Enter
+			this.createRoom();
+		}
+	}
+
+	createRoom() {
+		this.roomSrv.createRoom(this.newRoom).subscribe(
+			(room) => {
+				this.dialogRef.close('Room created');
+				this.userSrv.user.moddedRooms.push(room);
+				this.router.navigate([room.name]);
+			},
+			(error) => console.error(error)
+		);
+		this.newRoom;
+	}
+
+	isLogged(): boolean {
+		return this.userSrv.isLogged;
+	}
+
+	isAdmin(): boolean {
+		return this.userSrv.isAdmin;
 	}
 }
