ff65834bc6945b81230d34210968b229f19010ce
diegomzmn <diego_mzmn@hotmail.com>
2020-05-26 19:02:21 +0200

    Disable random jasmine execution

--- frontend/src/karma.conf.js
+++ frontend/src/karma.conf.js
@@ -12,10 +12,13 @@ module.exports = function (config) {
 			require('karma-coverage-istanbul-reporter'),
 			require('karma-notify-reporter'),
 			require('@angular-devkit/build-angular/plugins/karma'),
-			"karma-spec-reporter"
+			'karma-spec-reporter'
 		],
 		client: {
-			clearContext: false // leave Jasmine Spec Runner output visible in browser
+			clearContext: false, // leave Jasmine Spec Runner output visible in browser
+			jasmine: {
+				random: false
+			}
 		},
 		customLaunchers: {
 			ChromeHeadless: {
@@ -62,11 +65,11 @@ module.exports = function (config) {
 			'spec'
 		],
 		specReporter: {
-			maxLogLines: 5,         // limit number of lines logged per test
-			suppressErrorSummary: true,  // do not print error summary
-			suppressFailed: false,  // do not print information about failed tests
-			suppressPassed: false,  // do not print information about passed tests
-			suppressSkipped: true,  // do not print information about skipped tests
+			maxLogLines: 5, // limit number of lines logged per test
+			suppressErrorSummary: true, // do not print error summary
+			suppressFailed: false, // do not print information about failed tests
+			suppressPassed: false, // do not print information about passed tests
+			suppressSkipped: true, // do not print information about skipped tests
 			showSpecTiming: false // print the time elapsed for each spec
 		},
 		port: 9876,
