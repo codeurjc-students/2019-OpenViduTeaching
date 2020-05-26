dc352c34214888abfbe9f80e215f9d08958e187d
diegomzmn <diego_mzmn@hotmail.com>
2020-05-26 17:50:35 +0200

    Karma spec reporter added

--- frontend/src/karma.conf.js
+++ frontend/src/karma.conf.js
@@ -11,7 +11,8 @@ module.exports = function (config) {
 			require('karma-jasmine-html-reporter'),
 			require('karma-coverage-istanbul-reporter'),
 			require('karma-notify-reporter'),
-			require('@angular-devkit/build-angular/plugins/karma')
+			require('@angular-devkit/build-angular/plugins/karma'),
+			"karma-spec-reporter"
 		],
 		client: {
 			clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -57,8 +58,17 @@ module.exports = function (config) {
 			'kjhtml',
 			'dots',
 			// 'coverage-istanbul', // (https://github.com/mattlewis92/karma-coverage-istanbul-reporter/issues/49, https://github.com/angular/angular-cli/issues/10940)
-			'notify'
+			'notify',
+			'spec'
 		],
+		specReporter: {
+			maxLogLines: 5,         // limit number of lines logged per test
+			suppressErrorSummary: true,  // do not print error summary
+			suppressFailed: false,  // do not print information about failed tests
+			suppressPassed: false,  // do not print information about passed tests
+			suppressSkipped: true,  // do not print information about skipped tests
+			showSpecTiming: false // print the time elapsed for each spec
+		},
 		port: 9876,
 		colors: true,
 		logLevel: config.LOG_DEBUG,
