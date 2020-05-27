750d19a0b4ec419b2567558faea8d9275d011f19
diegomzmn <diego_mzmn@hotmail.com>
2020-05-27 13:04:11 +0200

    Revert "Travis fix try"
    This reverts commit 2a62486882d75d95830f1a0c4dabe0b0080625e6.

--- frontend/src/app/video/video.component.spec.ts
+++ frontend/src/app/video/video.component.spec.ts
@@ -3,8 +3,6 @@ import { VideoComponent } from './video.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SecurePipe } from '../shared/pipes/secure.pipe';
-import { OpenViduSessionServiceMock } from '../shared/services/openvidu-session/openvidu-session.service.mock';
-import { OpenViduSessionService } from '../shared/services/openvidu-session/openvidu-session.service';
 
 describe('VideoComponent', () => {
   let component: VideoComponent;
@@ -13,9 +11,6 @@ describe('VideoComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, RouterTestingModule],
-      providers: [
-				{provide: OpenViduSessionService, useClass: OpenViduSessionServiceMock},
-			],
       declarations: [ VideoComponent, SecurePipe ]
     })
     .compileComponents();
