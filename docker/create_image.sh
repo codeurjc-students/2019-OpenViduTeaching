b62ea3a178c1811be59b90117b52bb5c1496d865
Diego <diego_mzmn@hotmail.com>
2020-06-19 19:47:59 +0200

    Fix create_image.sh

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -2,6 +2,7 @@ cd ../frontend
 npm install
 npm audit fix
 npm run build --prod --baseHref=http://localhost:8080/
+cp -a dist/openvidu-teaching/. ../backend/src/main/resources/static/
 cd ../backend
 mvn -Dmaven.test.skip=true package
 cp target/OpenViduTeaching.jar ../docker/build/
