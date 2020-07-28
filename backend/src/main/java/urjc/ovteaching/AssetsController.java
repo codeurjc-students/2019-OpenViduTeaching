bb032577b5cea6681eaa71134369858919f8fd1d
diegomzmn <diego_mzmn@hotmail.com>
2020-07-28 22:35:33 +0200

    ASSETS_FOLDER as enviroment variable

--- backend/src/main/java/urjc/ovteaching/AssetsController.java
+++ backend/src/main/java/urjc/ovteaching/AssetsController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.NoSuchFileException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -23,10 +24,16 @@ public class AssetsController {
 	@Autowired
     ResourceLoader resourceLoader;
 	
+	@Value("${ASSETS_FOLDER}")
+	private String assetsFolder;
+	
 	@GetMapping("/assets/images/{image}")
 	public ResponseEntity<byte[]> getImage(@PathVariable String image) throws IOException {
 		try {
-			byte[] bytes = ByteStreams.toByteArray(resourceLoader.getResource("classpath:static/assets/images/" + image).getInputStream());
+			if (assetsFolder.startsWith("/") || assetsFolder.matches("[a-zA-Z]\\:.*")) {
+				assetsFolder = "file:" + assetsFolder;
+			}
+			byte[] bytes = ByteStreams.toByteArray(resourceLoader.getResource(assetsFolder + image).getInputStream());
 		
 			String[] imageName = image.split("\\.");
 			String filetype = imageName[imageName.length-1];
